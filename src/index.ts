2ec6af25d6c0ec2b1663c5eb0f19b5876e6d795d
Deva <devapandiyan19@gmail.com>
2023-08-13 05:19:39 +0530

    new chanes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export * from './components';
+// registry=https://registry.npmjs.org/.
+// @DevaPandiyan:registry=https://npm.pkg.github.com/
+// npm.pkg.github.com/:_authToken=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
